73cbbf54672ac4f36fa5efdf9563af827ffdaf76
Aislu Kharisova <arslanova1996ais@list.ru>
2023-12-28 18:40:28 +0300

    Проект express-mesto-gha на Node.js. Первая проверка. 14 Спринт.

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ app.post(
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().regex(pattern),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(pattern),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
